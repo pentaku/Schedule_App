4ed20de55ea989b7e95bdce8a19fbf33755cbf26
pentaku <ohara1728136@gmail.com>
2025-07-22 22:45:39 +0900

    routes.rbの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to : 'tasks#index'#トップページ設定
+  resources :tasks #tasksControllerをまとめて設定
 end
+
+  # get '/tasks', tasks#index                       # タスク一覧を表示
+  # get 'tasks/index', to: 'tasks#index'            # タスク一覧を表示
+  # get 'tasks/new', to: 'tasks#new'                # 新しいタスク作成フォームを表示
+  # post '/tasks', to: 'tasks#create'               # 新しいタスクを作成
+  # get '/tasks/:id', to: 'tasks#show'              # 特定のタスクを表示
+  # get '/tasks/:id/edit', to: 'tasks#edit'         # 特定のタスク編集フォームを表示
+  # patch '/tasks/:id', to: 'tasks#update'          # 特定のタスクを更新
+  # delete '/tasks/:id', to: 'tasks#destroy'        # 特定のタスクを削除
